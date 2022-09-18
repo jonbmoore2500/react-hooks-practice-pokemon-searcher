db8be913f61fa8e6fddc4a61cd5cc09930a3ce94
Jonathan <jonathanmoorevla@gmail.com>
2022-09-18 14:37:39 -0500

    Display all pokemon, handle front/back display

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard({pokemon}) {
+  const {id, name, hp, sprites} = pokemon
+  const [showFront, setShowFront] = useState(true)
   return (
     <Card>
       <div>
-        <div className="image">
-          <img alt="oh no!" />
+        <div className="image" onClick={() => setShowFront(!showFront)}>
+          {showFront ? 
+          <img alt={name} src={sprites.front}/> :
+          <img alt={name} src={sprites.back}/>
+          }
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {hp} hp
           </span>
         </div>
       </div>
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokemonArr, setPokemonArr] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:3001/pokemon')
+    .then(r => r.json())
+    .then(data => setPokemonArr(data))
+  }, [])
+  
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
@@ -13,7 +20,7 @@ function PokemonPage() {
       <br />
       <Search />
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemonArr={pokemonArr}/>
     </Container>
   );
 }
